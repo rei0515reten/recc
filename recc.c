bdafeab0dd796daf3e8e5f89a83148d0603426f6
Rei Otake <glay5152@gmail.com>
2021-10-25 23:02:08 +0900

    Add addition and subtraction

--- recc.c
+++ recc.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc,char **argv){
+int main(int argc,char **argv) {
   if(argc != 2){
     fprintf(stderr,"引数の数が正しくありません\n");
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax,%d\n",atoi(argv[1]));
+  //文字列を10進の数値に変換（&pには読み込んだ次の文字列のアドレス)
+  printf("  mov rax,%ld\n",strtol(p,&p,10));
+
+  //指し示す部分は上のstrtolでずれているため、ここでは文字列の二番目を指している
+  while(*p) {
+    if(*p == '+') {
+      p++;
+      printf("  add rax,%ld\n",strtol(p,&p,10));
+      continue;
+    }
+
+    if(*p == '-') {
+      p++;
+      printf("  sub rax,%ld\n",strtol(p,&p,10));
+      continue;
+    }
+
+    fprintf(stderr,"予期しない文字：%c\n",*p);
+  }
+
   printf("  ret\n");
   return 0;
 }
